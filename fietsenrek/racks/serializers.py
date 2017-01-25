6b4b5402bc96d2ef3c1841452eec48022b612b0e
Marta Rożek <marta.rozek@gmail.com>
2017-01-25 09:07:49 +0100

    Mark place_id as not required in rack serializer.

--- fietsenrek/racks/serializers.py
+++ fietsenrek/racks/serializers.py
@@ -4,6 +4,8 @@ from . import models
 
 
 class RackSerializer(serializers.ModelSerializer):
+    place_id = serializers.CharField(max_length=128, required=False)
+
     class Meta:
         model = models.Rack
         fields = '__all__'
